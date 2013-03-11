de6ee43ac9aa4cfd7899ab39630a39cdb1d640f4
vsasa <sasa.vranic@bring.out.ba>
2013-03-11 15:07:44 +0100

    fin tabele, dodane još 4 tabele

--- FMK2F18.sh
+++ FMK2F18.sh
@@ -17,7 +17,7 @@ cd $5
 TABLES=`ls *.dbf`
 for table in $TABLES ;do
     DBNAME=`echo $table | cut -d. -f1`
-/Users/bringout/Developer/harbour_playground/pgsql/dbf2pg -h $1 -y 5432 -d $4 -u $2 -p $3 -e fmk -f $table -t $DBNAME 
+    /Users/bringout/Developer/harbour_playground/pgsql/dbf2pg -h $1 -y 5432 -d $4 -u $2 -p $3 -e fmk -f $table -t $DBNAME 
 done
 exit
 
